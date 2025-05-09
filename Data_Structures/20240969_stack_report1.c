8587150013a0817b92ba446d2c4c674da25d56a8
Daniellll <shj1103s@naver.com>
2025-05-09 22:17:01 +0900

    update stackreport1 and add stackreport2

--- Data_Structures/20240969_stack_report1.c
+++ Data_Structures/20240969_stack_report1.c
@@ -82,46 +82,21 @@ void display(Stack *s) {
 	printf("\n");
 }
 
-int isMatching(char open, char close) {
-	if (open == '(' && close == ')') return 1;
-	if (open == '[' && close == ']') return 1;
-	if (open == '{' && close == '}') return 1;
-	return 0;
-}
-
 int main() {
 	Stack s;
 	char str[MAX];
 	initStack(&s, 5);
-	int isValid = 1;
+
 	printf("문자열 입력: ");
 	fgets(str, MAX, stdin);
 	str[strcspn(str, "\n")] = '\0';
 
-	for (int i = 0; str[i] != '\0'; i++) {
-		char ch = str[i];
-		if (ch == '(' || ch == '[' || ch == '{') {
-			push(&s, ch);
-		}
-		else if (ch == ')' || ch == ']' || ch == '}') {
-			if (isEmpty(&s) || !isMatching(pop(&s), ch)) {
-				isValid = 0;
-				break;
-			}
-		}
-	}
-	if (!isEmpty(&s)) isValid = 0;
-
-	if (isValid) printf("유효한 괄호\n");
-	else printf("유효하지 않은 괄호\n");
-
-	/*
+	for (int i = 0; str[i] != '\0'; i++) push(&s, str[i]);
 	printf("거꾸로 된 문자열 : ");
 	while (!isEmpty(&s)) {
 		printf("%c", pop(&s));
 	}
 	printf("\n");
-	*/
 
 	return 0;
 }
